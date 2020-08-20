cc9dc98a914e4ee8e4ab1d65c900fcd8faf92786
StevenDufour <sdufour6@myges.fr>
2020-08-20 16:33:07 +0200

    Update index routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,8 @@
 module.exports = function () {
     require('./auth.route')(...arguments);
+    require('./sub.route')(...arguments);
+    require('./event.route')(...arguments);
+    require('./product.route')(...arguments);
+    require('./site.route')(...arguments);
+    require('./user.route')(...arguments);
 }
