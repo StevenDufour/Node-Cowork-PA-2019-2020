108c4ab83b8f50e42d26e9c921a811df13dfb020
StevenDufour <sdufour6@myges.fr>
2021-02-06 17:40:05 +0100

    Correction

--- config/config.js
+++ config/config.js
@@ -10,19 +10,17 @@ module.exports = {
     "dialect": "postgres"
   },
   "test": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DB,
-    "host": process.env.POSTGRES_HOST,
-    "port": process.env.POSTGRES_PORT,
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.POSTGRES_USER,
-    "password": process.env.POSTGRES_PASSWORD,
-    "database": process.env.POSTGRES_DB,
-    "host": process.env.POSTGRES_HOST,
-    "port": process.env.POSTGRES_PORT,
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
     "dialect": "postgres"
   }
 }
