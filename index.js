8f46236b96b180a1c2c28b9ad21d6905d0a3eaa2
StevenDufour <sdufour6@myges.fr>
2021-02-07 14:09:11 +0100

    Add Cors

--- index.js
+++ index.js
@@ -2,15 +2,24 @@ require('dotenv').config();
 const express = require('express');
 const models = require('./models');
 const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
+app.options("*", cors());
+
+routes(app);
 
 async function bootstrap() {
     await models.sequelize.authenticate();
     await models.sequelize.sync();
 
-    const app = express();
-    routes(app);
 
-    app.use(function (req , res, next) {
+    app.use(function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         res.header("Access-Control-Allow-Methods", "GET,HEAD,POST,DELETE,PUT,OPTIONS");
