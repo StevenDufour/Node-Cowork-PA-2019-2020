141b3a09a471deb5e68ca8fdd697e489270914c5
StevenDufour <sdufour6@myges.fr>
2021-02-07 12:56:23 +0100

    Try to fix bug

--- utils/mailsender.util.js
+++ utils/mailsender.util.js
@@ -5,17 +5,17 @@ class MailsenderUtil {
     static async sendCreatedProductMail() {
 
         const transporter = AWS.createTransport({
-            host: process.env.AWS_SMTP_HOST,
-            secure: process.env.AWS_SMTP_SECURE,
-            port: process.env.AWS_SMTP_PORT,
+            host: "email-smtp.eu-west-3.amazonaws.com",
+            secure: true,
+            port: "587",
             auth: {
-                user: process.env.AWS_SMTP_USERNAME,
-                pass: process.env.AWS_SMTP_PASSWORD
+                user: "AKIAV37KVRO3V44RUAYE",
+                pass: "BFXyV1o/SiuKIEEswnW+E34R8zrWTh6S3Vpme7cW4ZzY"
             }
 
         });
         await transporter.sendMail({
-            from: process.env.AWS_SMTP_ADRESS, // sender address
+            from: "belmajkarim29@gmail.com", // sender address
             to: "kbelmajdoub1@myges.fr, sdufour6@myges.fr", // list of receivers
             subject: "New product was Created ", // Subject line
             text: "Hello we would love to inform you that a new product was created", // plain text body
