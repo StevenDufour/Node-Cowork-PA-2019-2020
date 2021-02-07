9eea30445df8898abfe462fba80102e9ae198052
StevenDufour <sdufour6@myges.fr>
2021-02-07 13:48:10 +0100

    Add app.use

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ async function bootstrap() {
     const app = express();
     routes(app);
 
+    app.use(function (req , res, next) {
+        res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "*");
+        res.header("Access-Control-Allow-Methods", "GET,HEAD,POST,DELETE,PUT,OPTIONS");
+        next();
+    });
+
     app.get('/', (req, res) => {
         res.send("Hello World");
     });
